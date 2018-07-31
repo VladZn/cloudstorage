890466f942548639e7d894a1d179096e0e35dc99
MAinframe\Vlad <vzinchenko36@gmail.com>
2018-07-31 11:35:19 +0300

    Network and DB improvements. Model class FileView for TableView control.

---
+++ client/src/main/java/ru/cloud/storage/client/FileView.java
@@ -0,0 +1,27 @@
+package ru.cloud.storage.client;
+
+import java.io.File;
+
+public class FileView {
+    private File file;
+    private String name;
+    private long size;
+
+    public FileView(File file) {
+        this.file = file;
+        this.name = file.getName();
+        this.size = file.length();
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public long getSize() {
+        return size;
+    }
+}
